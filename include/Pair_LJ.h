966d80a6d5aa73e325dd46f72ea0ae1d2a3992ee
gla_penn <gla_penn@hp83-inf-28.ictp.it>
2013-03-19 17:13:30 +0100

    Added Force Pair and Pair_LJ header files

---
+++ include/Pair_LJ.h
@@ -0,0 +1,39 @@
+/**
+ * Pair_LJ class
+ *
+ * @short This class provides what is needed to compute pair forces of Lennard-Jones type
+ * @authors Aris Marcolongo <XXX@gmail.com>
+ */
+
+#ifndef MD_PAIR_LJ_H
+#define MD_PAIR_LJ_H
+
+//Includes (TODO)
+#include "Pair.h"
+
+class Pair_LJ {
+
+    public:
+    /**
+     * Default constructor
+     * @param Pointer to atom class
+     */
+    Pair_LJ(double epsilon, double sigma);
+
+    /**
+     * Default destructor
+     */
+    virtual ~Pair_LJ();
+
+    /**
+     * Force computer
+     * @return Standard error code
+     */
+    void ComputeForce(Atom *atom);
+
+/* variables */
+    double sigma,epsilon;
+
+}
+
+#endif //> !class
