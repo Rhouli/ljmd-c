4b2da4527db23e1f41f3a1aa4092092ec3632c8f
gla_penn <gla_penn@hp83-inf-28.ictp.it>
2013-03-19 18:00:57 +0100

    Pair and Force Changes

---
+++ Include/Force.h
@@ -0,0 +1,55 @@
+/**
+ * Force class
+ *
+ * @short This class provides force calculations
+ * @authors Aris Marcolongo <XXX@gmail.com>
+ */
+
+#ifndef MD_FORCE_H
+#define MD_FORCE_H
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <math.h>
+#include "Helper.h"
+
+//Includes (TODO)
+#include "Atom.h"
+
+class Force {
+
+    public:
+    /**
+     * Default constructor
+     */
+    Force(){}
+
+    /**
+     * Default constructor
+     */
+    Force(char* pot, char* pot_type, double arg1, double arg2);
+
+    /**
+     * Default destructor
+     */
+    virtual ~Force();
+
+    /**
+     * Init
+     * @param Pointer to atom class
+     * @return Standard error code
+     */
+    static void ComputeForce(Atom* atom);
+
+    /**
+     * Calculate different kinds of pair-potentials
+     * @return a pointer to an object of class Pair
+    */
+    class Pair *pair;
+    char *pot;
+
+};
+
+#endif //> !class
---
+++ Include/Pair.h
@@ -0,0 +1,44 @@
+/**
+ * Pair class
+ *
+ * @short This class provides what is needed to compute pair forces
+ * @authors Aris Marcolongo <XXX@gmail.com>
+ */
+
+#ifndef MD_PAIR_H
+#define MD_PAIR_H
+
+//Includes (TODO)
+#include "Force.h"
+
+class Pair {
+
+    public:
+    /**
+     * Default constructor
+     */
+    Pair(){}
+
+    /**
+     * Full constructor
+     */
+    Pair(char* pot_type, double arg1, double arg2);
+
+    /**
+     * Default destructor
+     */
+    virtual ~Pair();
+
+    /**
+     * Init
+     * @param Pointer to atom class
+     * @return Standard error code
+     */
+    static void ComputeForce(Atom *atom);
+    class Pair_LJ* LJ;
+//    class Morse* Pair_Morse;
+    char *pot_type;
+
+};
+
+#endif //> !class
