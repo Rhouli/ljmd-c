40ea355b6f04c05877f30395ff888f60b9b0ec9f
Iggy <iggy@ThinkpadX1.(none)>
2013-03-20 08:58:16 -0400

    Renamed

---
+++ INC/Pair_LJ.h
@@ -0,0 +1,37 @@
+/**
+ * Pair_LJ class
+ *
+ * @short This class provides what is needed to compute pair forces of Lennard-Jones type
+ * @authors Aris Marcolongo <XXX@gmail.com>
+ */
+
+#ifndef MD_PAIR_LJ_H
+#define MD_PAIR_LJ_H
+
+#include "Pair.h"
+
+class Pair_LJ {  
+ public:
+  Pair_LJ(){}
+  /**
+   * Default constructor
+   * @param Pointer to atom class
+   */
+  Pair_LJ(double epsilon, double sigma);
+    
+  /**
+   * Default destructor
+   */
+  virtual ~Pair_LJ(){}
+  
+  /**
+   * Force computer
+   * @return Standard error code
+   */
+  void ComputeForce(Atom *atom);
+  
+  /* variables */
+  double sigma,epsilon;
+};
+
+#endif //> !class
