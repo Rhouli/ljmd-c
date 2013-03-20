d9d4dcccff9200872b4a662dc596a58a1f960a35
gla_penn <gla_penn@hp83-inf-29.ictp.it>
2013-03-20 17:37:05 +0100

    Changed the Pair_LJ.cpp FIle

--- SRC/Pair_LJ.cpp
+++ SRC/Pair_LJ.cpp
@@ -12,79 +12,79 @@
 #endif
 
 /* construction */
-Pair_LJ::Pair_LJ(double e, double s) {
-  epsilon=e;
-  sigma=s;
-}
+Pair_LJ::Pair_LJ(){
+	Pair_LJ(double e, double s) {
+		epsilon=e;
+		sigma=s;
+	}
 
-static void Pair_LJ::ComputeForce(Atom *sys) 
+static void Pair_LJ::ComputeForce(Atom *atom) 
 {
-  double epot;
-  
-  epot = 0.0;
-  
+    double epot;
+    
 #if defined(_OPENMP)
 #pragma omp parallel reduction(+:epot)
 #endif
-  {
+    {
         double c12,c6,boxby2,rcsq;
-        double *f;
+        double *fx, *fy, *fz;
         const double *rx, *ry, *rz;
-        int i, tid, fromidx, toidx, natoms;
-	
+        int i, tid, fromidx, toidx, natoms, nthreads;
+
         /* precompute some constants */
         c12 = 4.0*epsilon*pow(sigma,12.0);
         c6  = 4.0*epsilon*pow(sigma, 6.0);
-        rcsq= sys->rcut * sys->rcut;
-        boxby2 = 0.5*sys->box;
-        natoms = sys->natoms;
+        rcsq= atom->GetRadCut() * atom->GetRadCut();
+        boxby2 = 0.5*atom->GetBoxSize();
+        natoms = atom->GetNAtoms();
         epot = 0.0;
         
         /* let each thread operate on a different
            part of the enlarged force array */
 #if defined(_OPENMP)
+	nthreads=omp_get_num_threads();
         tid=omp_get_thread_num();
 #else
+	nthreads=1;
         tid=0;
 #endif
-        f=sys->getForceArray;
-	fx=f + (3*tid*natoms);
-	fx=sys->frc;
+        fx=atom->GetForce() + (3*tid*natoms);
         azzero(fx,3*natoms);
-        fy=sys->frc + ((3*tid+1)*natoms);
-        fz=sys->frc + ((3*tid+2)*natoms);
-        rx=sys->pos;
-        ry=sys->pos + natoms;
-        rz=sys->pos + 2*natoms;
+        fy=atom->GetForce() + ((3*tid+1)*natoms);
+        fz=atom->GetForce() + ((3*tid+2)*natoms);
+        rx=atom->GetPosition();
+        ry=atom->GetPosition() + natoms;
+        rz=atom->GetPosition() + 2*natoms;
 
         /* self interaction of atoms in cell */
-        for(i=0; i < sys->ncell; i += sys->nthreads) {
+        for(i=0; i < atom->GetNCells(); i += nthreads) {
             int j;
             const cell_t *c1;
-
-            j = i + tid;
-            if (j >= (sys->ncell)) break;
-            c1=sys->clist + j;
-
-            for (j=0; j < c1->natoms-1; ++j) {
+            
+            x = i + tid;
+            if (j >= (atom->GetNCells())) break;
+//            c1=atom->clist + j;
+            afc=getlist(j) 
+            for (j=0; j < atom->GetCellSize(x)-1; ++j) {
+//	    for (j=0; j < GetCellData()-1; ++j) {  
                 int ii,k;
-                double rx1, ry1, rz1;
-
-                ii=c1->idxlist[j];
+                double rx1, ry1, rz1;		
+ //               ii=c1->idxlist[j];
+		ii=atom->GetCellData(x,j)
+		
                 rx1=rx[ii];
                 ry1=ry[ii];
                 rz1=rz[ii];
         
-                for(k=j+1; k < c1->natoms; ++k) {
+                for(k=j+1; k < atom->GetCellSize(x); ++k) {
                     int jj;
                     double rx2,ry2,rz2,rsq;
 
-                    jj=c1->idxlist[k];
-
+		    jj=atom->GetCellData(x,k) 
                     /* get distance between particle i and j */
-                    rx2=pbc(rx1 - rx[jj], boxby2, sys->box);
-                    ry2=pbc(ry1 - ry[jj], boxby2, sys->box);
-                    rz2=pbc(rz1 - rz[jj], boxby2, sys->box);
+                    rx2=pbc(rx1 - rx[jj], boxby2, atom->box);
+                    ry2=pbc(ry1 - ry[jj], boxby2, atom->box);
+                    rz2=pbc(rz1 - rz[jj], boxby2, atom->box);
                     rsq = rx2*rx2 + ry2*ry2 + rz2*rz2;
 
                     /* compute force and energy if within cutoff */
@@ -109,34 +109,36 @@ static void Pair_LJ::ComputeForce(Atom *sys)
         }    
 
         /* interaction of atoms in different cells */
-        for(i=0; i < sys->npair; i += sys->nthreads) {
+        for(i=0; i < atom->GetNPair(); i += atom->nthreads) {
             int j;
             const cell_t *c1, *c2;
 
-            j = i + tid;
-            if (j >= (sys->npair)) break;
-            c1=sys->clist + sys->plist[2*j];
-            c2=sys->clist + sys->plist[2*j+1];
+            x = i + tid;
+            if (x >= (atom->GetNPair())) break;
+	  
+        //    c1=atom->clist + atom->plist[2*x];
+        //    c2=atom->clist + atom->plist[2*x+1];
         
-            for (j=0; j < c1->natoms; ++j) {
+            for (j=0; j < atom->GetCellSize(atom->GetPairItem(2*x)); ++j) {
                 int ii, k;
                 double rx1, ry1, rz1;
 
-                ii=c1->idxlist[j];
+                ii=atom->GetCellData(atom->GetPairItem(2*x),j);
                 rx1=rx[ii];
                 ry1=ry[ii];
                 rz1=rz[ii];
         
-                for(k=0; k < c2->natoms; ++k) {
+                for(k=0; k < atom->GetCellSize(atom->GetPairItem(2*x+1)); ++k) {
                     int jj;
                     double rx2,ry2,rz2,rsq;
                 
-                    jj=c2->idxlist[k];
-                
+                   // jj=c2->idxlist[k];
+		    
+		    jj = atom->GetCellData(atom->GetPairItem(2*x+1),k);
                     /* get distance between particle i and j */
-                    rx2=pbc(rx1 - rx[jj], boxby2, sys->box);
-                    ry2=pbc(ry1 - ry[jj], boxby2, sys->box);
-                    rz2=pbc(rz1 - rz[jj], boxby2, sys->box);
+                    rx2=pbc(rx1 - rx[jj], boxby2, atom->box);
+                    ry2=pbc(ry1 - ry[jj], boxby2, atom->box);
+                    rz2=pbc(rz1 - rz[jj], boxby2, atom->box);
                     rsq = rx2*rx2 + ry2*ry2 + rz2*rz2;
                 
                     /* compute force and energy if within cutoff */
@@ -166,7 +168,7 @@ static void Pair_LJ::ComputeForce(Atom *sys)
 #pragma omp barrier
 #endif
         /* set equal chunks of index ranges */
-        i = 1 + (3*sys->natoms / sys->nthreads);
+        i = 1 + (3*natoms / nthreads);
         fromidx = tid * i;
         toidx = fromidx + i;
         if (toidx > 3*natoms) toidx = 3*natoms;
@@ -174,15 +176,18 @@ static void Pair_LJ::ComputeForce(Atom *sys)
         /* reduce forces from threads with tid != 0 into
            the storage of the first thread. since we have
            threads already spawned, we do this in parallel. */
-        for (i=1; i < sys->nthreads; ++i) {
+        for (i=1; i < nthreads; ++i) {
             int offs, j;
 
             offs = 3*i*natoms;
 
             for (j=fromidx; j < toidx; ++j) {
-                sys->SetForce(j, sys->GetForce(j)+sys->GetForce(offs+j));
+  //              atom->frc[j] += atom->frc[offs+j];
+		atom->SetForce(j,atom->GetForce(j)+atom->GetForce(offs+j)); 
             }
         }
-  }
-  sys->SetPotEnergy(epot);
+    }
+    atom->SetPotEnergy(epot);
+}
+
 }
