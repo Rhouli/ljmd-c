456430246caf407c1d59f382b92e0e36e4e4217f
Leopold Talirz <leopold.talirz@gmail.com>
2013-03-20 13:48:24 +0100

    Added info on built-in ASE viewer
    The ASE viewer allows to visualize Atoms objects
    directly in python.

--- python/examples/ex01/ex01.py
+++ python/examples/ex01/ex01.py
@@ -25,3 +25,9 @@ myatoms.set_calculator(calc)
 # Get the total energy at the end of the run
 etot = myatoms.get_total_energy()
 print "The total energy is {} [unit] per atom".format(etot/len(myatoms))
+
+# If the pygtk module is available, you can use the
+# built-in ASE viewer to visualize the atoms
+
+#from ase.visualize import view
+#view(myatoms)
--- python/setup.py
+++ python/setup.py
@@ -1,5 +1,14 @@
+#!/usr/bin/env python
+
 from distutils.core import setup
-setup(name='mymd',
-      version='0.0.1',
-      py_modules=['mymd'],
+
+setup(
+      name='MyMD Python Frontend',
+      version='0.1',
+      description='Python Frontend for simple MD program',
+      author='Leopold Talirz',
+      author_email='leopold.talirz@gmail.com',
+#      url='http://www.python.org/sigs/distutils-sig/',
+      packages=['ase.calculators.mymd'],
+#      py_modules=['mymd'],
       )
